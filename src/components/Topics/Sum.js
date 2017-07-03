e9b99b99997bbe8c29057782836857543829d0fd
MeganMFisher <meganmichelle318@gmail.com>
2017-07-03 11:43:56 -0600

    sum working!

--- src/components/Topics/Sum.js
+++ src/components/Topics/Sum.js
@@ -1,13 +1,37 @@
 import React, { Component } from 'react';
 
 export default class Sum extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      number1: 0,
+      number2: 0,
+      sum: null
+    }
+  }
+
+  updateNumberOne(val) {
+    this.setState({ number1: parseInt(val, 10) })
+  }
+
+  updateNumberTwo(val) {
+    this.setState({ number2: parseInt(val, 10) })
+  }
+
+  add(num1, num2) {
+    this.setState({ sum: num1 + num2 })
+  }
+
+
   render() {
     return (
-      <div className="puzzleBox filterObjectPB">
+      <div className="puzzleBox sumPB">
         <h4> Sum </h4>
-
-        <input className="inputLine"></input>
-        <button className="confirmationButton"></button>
+        <input className="inputLine" type='number' onChange={ (e) => this.updateNumberOne(e.target.value) }></input>
+        <input className="inputLine" type='number' onChange={ (e) => this.updateNumberTwo(e.target.value) }></input>
+        <button className="confirmationButton" onClick={ () => this.add(this.state.number1, this.state.number2) }> Sum </button>
+        <span className='resultsBox'> Sum: { this.state.sum } </span>
       
       </div>
     )
